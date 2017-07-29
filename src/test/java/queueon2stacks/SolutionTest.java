3ee432c7a635fe0a6a4ff7bbed837d7fa8368106
nameofthelaw <tsibin.andr@gmail.com>
2017-07-29 10:22:00 +0300

    feat: Add queue on 2 stacks task

---
+++ src/test/java/queueon2stacks/SolutionTest.java
@@ -0,0 +1,13 @@
+package queueon2stacks;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SolutionTest {
+
+    @Test
+    void test1() {
+        Solution.main("10 1 42 2 1 14 3 1 28 3 1 60 1 78 2 2".split(" "));
+    }
+}
